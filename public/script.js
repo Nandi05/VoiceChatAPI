55f953e8e3f3051c7d77472d13c4844a9af41db1
Nandini <nandiniwadhwa08@gmail.com>
2022-11-17 02:33:39 +0530

    Added my project

---
+++ public/script.js
@@ -0,0 +1,40 @@
+const socket=io('/');
+const videoGrid=document.getElementById('video-grid');
+const myVideo=document.createElement('video');
+myVideo.muted=true;
+
+var peer=new Peer(undefined,{
+    path: '/peerjs',
+    host: '/',
+    port:'3030'
+});
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video:true, // set false for audio chat
+    audio:true
+}).then(stream =>{
+    myVideoStream=stream;
+    addVideoStream(myVideo,stream);
+})
+peer.on('open',id=>{
+    socket.emit('join-room',ROOM_ID, id);
+});
+
+socket.on('user-connected',(userId)=>{
+    connectToNewUser(userId);
+})
+
+const connectToNewUser = (userId) =>{
+    console.log('new user!' , userId);
+}
+
+
+
+const addVideoStream=(video,stream)=>{
+    video.srcObject=stream;
+    video.addEventListener('loadedmetadata',()=>{
+        video.play();
+    })
+    videoGrid.append(video);
+}
